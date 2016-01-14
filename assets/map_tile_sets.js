4d64c65067f19f4eada313d8d287337cacad4d82
DiegoRGonzalez <drg4@williams.edu>
2016-01-13 23:24:11 -0500

    Dev has graphics

--- assets/map_tile_sets.js
+++ assets/map_tile_sets.js
@@ -1,10 +1,11 @@
 Game.MapTileSets = {
   caves1: {
-    _width: 300,
-    _height: 200,
+    _width: 100,
+    _height: 100,
     getMapTiles: function () {
       var mapTiles = Game.util.init2DArray(this._width, this._height, Game.Tile.nullTile);
       var generator = new ROT.Map.Cellular(this._width, this._height);
+      generator._options.connected = true;
       generator.randomize(0.5);
 
       //repeated cellular automata process
@@ -16,13 +17,44 @@ Game.MapTileSets = {
       // run again then update map
       generator.create(function(x,y,v) {
         if (v === 1) {
-          mapTiles[x][y] = Game.Tile.floorTile;
-        } else {
           mapTiles[x][y] = Game.Tile.wallTile;
+        } else {
+          mapTiles[x][y] = Game.Tile.floorTile;
         }
       });
 
       return mapTiles;
     }
-  }
+
+    },
+
+    maze: {
+        _width: 100,
+        _height: 100,
+        getMapTiles: function () {
+          var mapTiles = Game.util.init2DArray(this._width, this._height, Game.Tile.nullTile);
+          var generator = new ROT.Map.Digger(this._width, this._height);
+        //  generator.randomize(0.5);
+
+        //   //repeated cellular automata process
+        //   var totalIterations = 3;
+        //   for (var i = 0; i < totalIterations - 1; i++){
+        //     generator.create();
+        //   }
+
+          // run again then update map
+          generator.create(function(x,y,v) {
+            if (v === 1) {
+              mapTiles[x][y] = Game.Tile.wallTile;
+            } else {
+              mapTiles[x][y] = Game.Tile.floorTile;
+            }
+          });
+
+          return mapTiles;
+        }
+
+    }
+
+
 };
