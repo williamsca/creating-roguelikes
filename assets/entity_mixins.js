0088a861c38eeefff62c182a13589a32dcb6d33e
Colin Williams <Colin Williams>
2016-01-13 23:27:20 -0500

    um

--- assets/entity_mixins.js
+++ assets/entity_mixins.js
@@ -9,24 +9,30 @@ Game.EntityMixin.WalkerCorporeal = {
   tryWalk: function(map, dx, dy) {
     var targetX = Math.min(Math.max(0, this.getX() + dx), map.getWidth());
     var targetY = Math.min(Math.max(0, this.getY() + dy), map.getHeight());
+
+    // INTERACT WITH ENTITY
     if (map.getEntity(targetX, targetY)) {
-      return false;
+      this.raiseEntityEvent('bumpEntity', {actor: this, recipient:map.getEntity(targetX, targetY)});
+      this.raiseEntityEvent('tookTurn');
+      return true;
     }
+
+    // TRAVEL
     if (map.getTile(targetX, targetY).isWalkable()) {
       newPos = {
         x: targetX,
         y: targetY
-      }
+      };
       this.setPos(newPos);
       var myMap = this.getMap();
       if (myMap) {
         myMap.updateEntityLocation(this);
       }
-      if (this.hasMixin('Chronicle')) {
-        this.trackTurn();
-      }
+      this.raiseEntityEvent('tookTurn');
       return true;
     }
+
+    // YOU CAN'T WALK INTO A WALL
     return false;
   }
 };
@@ -38,7 +44,21 @@ Game.EntityMixin.Chronicle = {
     mixinGroup: 'Chronicle',
     stateNamespace: '_Chronicle_attr',
     stateModel: {
-      turnCounter: 0
+      turnCounter: 0,
+      killLog: {},
+      deathMessage: ''
+    },
+    listeners: {
+      'tookTurn': function(evtData) {
+        this.trackTurn();
+      },
+      'madeKill': function(evtData) {
+        console.log("chronicle kill");
+        this.addKill(evtData.entKilled);
+      },
+      'killed': function(evtData) {
+        this.attr._Chronicle_attr.deathMessage = 'killed by ' + evtData.killedBy.getName();
+      }
     }
   },
   trackTurn: function() {
@@ -49,6 +69,21 @@ Game.EntityMixin.Chronicle = {
   },
   setTurns: function(n) {
     this.attr._Chronicle_attr.turnCounter = n;
+  },
+  getKills: function() {
+    return this.attr._Chronicle_attr.killLog;
+  },
+  clearKills: function() {
+    this.attr._Chronicle_attr.killLog = {};
+  },
+  addKill: function(entKilled) {
+    var entName = entKilled.getName();
+    console.log('chonicle kill of ' + entName);
+    if (this.attr._Chronicle_attr[entName]) {
+      this.attr._Chronicle_attr.killLog[entName]++;
+    } else {
+      this.attr._Chronicle_attr.killLog[entName] = 1;
+    }
   }
 };
 
@@ -65,6 +100,29 @@ Game.EntityMixin.HitPoints = {
     init:function(template) {
       this.attr._HitPoints_attr.maxHp = template.MaxHp || 1;
       this.attr._HitPoints_attr.curHp = template.curHp || this.attr._HitPoints_attr.maxHp;
+    },
+    listeners: {
+      'attacked': function(evtData) {
+        console.log("HitPoints attacked");
+
+        this.takeHits(evtData.attackPower);
+        this.raiseEntityEvent('damagedBy',
+          {damager: evtData.attack, damageAmount: evtData.attackPower});
+        evtData.attacker.raiseEntityEvent('dealtDamage',
+          {damagee: this, damageAmount: evtData.attackPower});
+
+        // DEATH
+        if (this.getCurHp() <= 0) {
+          this.raiseEntityEvent('killed',
+            {entKilled: this, killedBy: evtData.attacker});
+          evtData.attacker.raiseEntityEvent('madeKill',
+            {entKilled: this, killedBy: evtData.attacker});
+        }
+      },
+      'killed': function(evtData) {
+        console.log('HitPoints killed');
+        this.destroy();
+      }
     }
   },
   getMaxHp: function () {
@@ -85,5 +143,28 @@ Game.EntityMixin.HitPoints = {
   recoverHits: function (amt) {
     this.attr._HitPoints_attr.curHp = Math.min(this.attr._HitPoints_attr.curHp + amt, this.attr._HitPoints_attr.maxHp);
   }
+};
 
+// MELEE COMBAT
+Game.EntityMixin.MeleeAttacker = {
+  META: {
+    mixinName: 'MeleeAttacker',
+    mixinGroup: 'Attacker',
+    stateNamespace: '_MeleeAttacker_attr',
+    stateModel: {
+      attackPower: 1
+    },
+    init: function (template) {
+      this.attr._MeleeAttacker_attr.attackPower = template.attackPower || 1;
+    },
+    listeners: {
+      'bumpEntity': function(evtData) {
+        console.log('MeleeAttacker bumpEntity');
+        evtData.recipient.raiseEntityEvent('attacked', {attacker: evtData.actor, attackPower: this.getAttackPower()});
+      }
+    }
+  },
+  getAttackPower: function() {
+    return this.attr._MeleeAttacker_attr.attackPower;
+  }
 };
